6f0c82e75d8139e069cdb7fc8cac86be37caa41c
ajithofficial <ajith.klu.1997@gmail.com>
2024-03-07 17:27:18 +0530

    Integrated Functionalities

---
+++ script.js
@@ -0,0 +1,70 @@
+const cellElements = document.querySelectorAll('[grid-cell]');
+const winnerText = document.querySelector('[winner-text]');
+const winnerElement = document.getElementById('winner-box');
+const startTextElement = document.getElementById('start-box');
+const restartElement = document.getElementById('restart');
+
+let circleTurn;
+const CLASSLIST = {
+    circle: 'circle',
+    cross: 'cross'
+};
+const conditions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
+const handleClick = (e) => {
+    // place mark
+    const cell = e.target;
+    const currentClass = circleTurn ? CLASSLIST.circle: CLASSLIST.cross;
+    changeClass(cell, currentClass);
+    // check win
+    if(checkForWin(currentClass)) {
+        winnerText.innerText = `${circleTurn ? 'Cross': 'Circle'} Won!`;
+        winnerElement.classList.add('show');
+    } else if(checkDraw()) {
+        winnerText.innerText = `Match Draw!`;
+        winnerElement.classList.add('show');
+    }
+};
+
+const changeClass = (cell, className) => {
+    if(!cell.classList.value.includes(CLASSLIST.circle) && !cell.classList.value.includes(CLASSLIST.cross)) {
+        circleTurn = !circleTurn;
+        cell.classList.add(className);
+        startTextElement.classList.add('hide')
+    }
+}
+
+const checkForWin = (currentClass) => {
+    return conditions.some(condition => {
+        return condition.every(index => {
+            if(cellElements[index]) {
+                return cellElements[index].classList.contains(currentClass)
+            }
+        });
+    })
+}
+const checkDraw = () => {
+    return [...cellElements].every((cell) => (cell.classList.contains(CLASSLIST.circle) || cell.classList.contains(CLASSLIST.cross)));
+}
+
+const handleReStart = () => {
+    winnerElement.classList.remove('show');
+    startGame();
+}
+
+
+const startGame = () => {
+    circleTurn = true;
+    cellElements.forEach((cell) => {
+        cell.classList.remove(CLASSLIST.circle);
+        cell.classList.remove(CLASSLIST.cross);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    startTextElement.classList.remove('hide')
+    restartElement.addEventListener('click', handleReStart);
+}
+startGame();
